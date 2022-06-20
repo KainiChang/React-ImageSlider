b425e774f36ef9ebcc761420c7646e4023dbc984
KainiChang <changkaini91@gmail.com>
2022-06-20 15:13:19 +0930

    update

--- src/slider/Btn.js
+++ src/slider/Btn.js
@@ -1,13 +1,13 @@
-import React from "react";
-import "./Slider.css";
-import leftArrow from "./icons/left-arrow.svg";
-import rightArrow from "./icons/right-arrow.svg";
+// import React from "react";
+// import "./Slider.css";
+// import leftArrow from "./icons/left-arrow.svg";
+// import rightArrow from "./icons/right-arrow.svg";
 
-export default function Btn(moveSlide,direction){
+// export default function Btn(moveSlide,direction){
 
-return(
-    <button className={direction==="prev"?"btn-slide-prev":"btn-slide-next"} onClick={moveSlide}>
-        <img  src= {direction==="prev"?leftArrow:rightArrow} alt="button"/>
-    </button>
-)
-}
+// return(
+//     <button className={direction==="prev"?"btn-slide-prev":"btn-slide-next"} onClick={moveSlide}>
+//         <img  src= {direction==="prev"?leftArrow:rightArrow} alt="button" className='btn-slide-img' />
+//     </button>
+// )
+// }
